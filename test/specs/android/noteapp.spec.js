229fdaef57785ae8e2260145e4cc559b8a1a3c84
Osim Khusainov <khusainov-o@pg.com>
2023-02-01 20:57:51 -0500

    test failure result

--- test/specs/android/noteapp.spec.js
+++ test/specs/android/noteapp.spec.js
@@ -2,7 +2,7 @@ import { addLog } from '../../helpers/commands.js';
 
 describe('test', () => {
   it('test', async function () {
-    await expect(2).toEqual(2);
+    await expect(2).toEqual(1);
     addLog('Said Hi');
     console.log('hi');
   });
